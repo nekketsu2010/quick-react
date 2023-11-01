07a696884405d08c867a6fb459c91126797b7832
nekketsu2010 <>
2023-11-01 22:22:40 +0900

    feature: isNewがtrueの場合にアイコンを表示するようにしました。

--- src/MyArticle.js
+++ src/MyArticle.js
@@ -1,3 +1,5 @@
+import MyNew from "./MyNew";
+
 export default function MyArticle(props) {
     return (
         /*b. dt, dd要素をくくるダミーの要素*/
@@ -6,6 +8,7 @@ export default function MyArticle(props) {
                 <a href={props.url}>
                     {props.title}
                 </a>
+                { props.isNew ? <MyNew /> : null }
             </dt>
             <dd>
                 {props.description}
---
+++ src/MyNew.js
@@ -0,0 +1,5 @@
+import icon_new from './new.gif'
+
+export default function MyNew() {
+    return <img src={icon_new} alt="new" />
+}
