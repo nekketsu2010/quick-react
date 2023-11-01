6877f970135e0a70bcfcd983f31e49f5218c1fd4
nekketsu2010 <>
2023-11-02 07:46:00 +0900

    feature: イベントハンドラーに引数を付けてみた。 アロー関数にすることで、引数付きのイベントハンドラーになる。

--- src/MyEvent.js
+++ src/MyEvent.js
@@ -1,15 +1,15 @@
 export default function MyEvent(props) {
     // 入力ボックス変更時にメッセージを出力
-    function show(e) {
+    function show(e, suffix) {
         // b. greet属性、入力値に基づいて、メッセージを生成
-        console.log(`${props.greet}, ${e.target.value}!!`);
+        console.log(`${props.greet}, ${e.target.value} ${suffix}!!`);
     }
 
     return (
         <form>
             <label htmlFor="txtName">名前：</label>
             {/* a. 入力変更時に show メソッドを呼び出し */}
-            <input id="txtName" type="text" onChange={show} />
+            <input id="txtName" type="text" onChange={e => show(e, 'さん')} />
         </form>
     )
 }
