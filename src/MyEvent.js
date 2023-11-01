c788f46225746688b93414c2c16b9117f0f5ea7a
nekketsu2010 <>
2023-11-02 07:51:24 +0900

    feature: データ属性を使用した方法で記載しました。

--- src/MyEvent.js
+++ src/MyEvent.js
@@ -1,15 +1,14 @@
 export default function MyEvent(props) {
-    // 入力ボックス変更時にメッセージを出力
-    function show(e, suffix) {
-        // b. greet属性、入力値に基づいて、メッセージを生成
-        console.log(`${props.greet}, ${e.target.value} ${suffix}!!`);
+    // b. データ属性経由で追加データを取得
+    function show(e) {
+        console.log(`${props.greet}, ${e.target.value} ${e.target.dataset.suffix}!!`);
     }
 
     return (
         <form>
             <label htmlFor="txtName">名前：</label>
             {/* a. 入力変更時に show メソッドを呼び出し */}
-            <input id="txtName" type="text" onChange={e => show(e, 'さん')} />
+            <input id="txtName" type="text" data-suffix="さん" onChange={show} />
         </form>
     )
 }
