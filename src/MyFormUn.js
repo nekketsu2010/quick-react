0ec4292d3cee8611dcfa6f77eecd22cd498f3c2e
nekketsu2010 <>
2023-11-06 23:27:01 +0900

    feature: 非制御コンポーネントでチェックボックスの値を管理してみました。

--- src/MyFormUn.js
+++ src/MyFormUn.js
@@ -1,13 +1,15 @@
 import { useRef } from "react";
 
-export default function MyForm() {
+export default function MyFormUn() {
     // a. React要素に紐つける参照を準備
     const name = useRef(null);
-    const email = useRef(null);    
+    const email = useRef(null);   
+    const checkbox = useRef(null);
     // c. 参照経由で入力値を取得
     function show() {
         console.log(`name: ${name.current.value}`);
         console.log(`email: ${email.current.value}`);
+        console.log(`checkbox: ${checkbox.current.checked}`);
     }
     // フォームを描画
     return (
@@ -20,6 +22,10 @@ export default function MyForm() {
             <div>
             <label htmlFor="email">メールアドレス：</label>
             <input id="email" name="email" type="mail" ref={email} defaultValue="tyamada@example.com" />
+            </div>
+            <div>
+                <input id="checkbox" type="checkbox" name="checkbox" ref={checkbox} defaultChecked />
+                <label htmlFor="checkbox">チェックボックス</label>
             </div>
             <div>
                 <button type="button" onClick={show}> 送信 </button>
