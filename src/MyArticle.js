8c5b080b03cb7570eab31df73bffcd28a10afe53
nekketsu2010 <>
2023-11-13 20:42:57 +0900

    fix: 不要なパラメータの削除

--- src/MyArticle.js
+++ src/MyArticle.js
@@ -1,13 +1,7 @@
-import MyNew from "./MyNew";
 import { useParams } from 'react-router-dom';
 
 export default function MyArticle() {
     const params = useParams();
-    function renderIfNew(isNew) {
-        if (isNew) {
-            return <MyNew />
-        }
-    }
 
     return (
         /*b. dt, dd要素をくくるダミーの要素*/
