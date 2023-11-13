384a08a01f6148c4cd1655d40d0cbe918832377c
nekketsu2010 <>
2023-11-13 20:02:15 +0900

    feature: スタイルクラスを適用した。

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { NavLink, Outlet } from 'react-router-dom';
+import './App.css';
 
 // b. スタイルを決定するための関数
-function isCurrent(link) {
-  return link.isActive ? { backgroundColor: 'Yellow' } : undefined;
+function isCurrent({ isActive }) {
+  return isActive ? 'current' : undefined;
 }
 
 export default function App() {
@@ -10,9 +11,9 @@ export default function App() {
     <>
       <ul>
         {/* a. 現在のページを表すリンクに対してスタイルを適用 */}
-        <li><NavLink style={isCurrent} end to="/">トップ</NavLink></li>
-        <li><NavLink style={isCurrent} to="/hello">Hello</NavLink></li>
-        <li><NavLink style={isCurrent} to="/article">公開記事</NavLink></li>  
+        <li><NavLink end className={ isCurrent } to="/">トップ</NavLink></li>
+        <li><NavLink className={ isCurrent } to="/hello">Hello</NavLink></li>
+        <li><NavLink className={ isCurrent } to="/article">公開記事</NavLink></li>  
       </ul>
       <hr />
       <Outlet />
