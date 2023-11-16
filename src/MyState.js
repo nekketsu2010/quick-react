47027be495b1d3f0708c9beeaed8b7e4cd7d0d71
nekketsu2010 <>
2023-11-16 18:29:28 +0900

    feature: MyStateにuseEffectを追加し、描画されるごとにカウント回数をログ出力するようにした。

--- src/MyState.js
+++ src/MyState.js
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function MyState(props) {
     // a. Stateの初期値を設定
     const [count, setCount] = useState(0);
+
+    // コンポーネントの（再）描画時に実行
+    useEffect(() => {
+        console.log(`${count}回、クリックされました。`);
+    });
+
     // c. count値をインクリメント（イベントハンドラー）
     function increment() {
         setCount(count + 1);
